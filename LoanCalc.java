febe550333791e8b1ec431fdea68ee66c5e7127b
asafaloush <asafaloush@gmail.com>
2024-11-26 23:38:05 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -30,7 +30,7 @@ public class LoanCalc {
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		for (int i = 0; i < n; i++){
 			loan = loan - payment;
-			loan = loan * rate;
+			loan = loan + ((rate/100)*loan);
 		}
 		return loan;
 	}
